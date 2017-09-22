0bf9e960e5c926706e6d2061d504dbb81e813b06
“fantazer” <kuznecovs45@gmail.com>
2017-09-22 13:15:47 +0300

    asdasd

---
+++ stock/index copy.php
@@ -0,0 +1,94 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Акции3");
+$APPLICATION->SetPageProperty("bg-header", "baner-stock.jpg");
+//test 5888 99
+?>
+<form id="myform">
+<input type="text" name="field1" id="field1" class="required" />
+<input type="submit" />
+</form>
+<div class="sale-block-wrap">
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 1</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 2</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 3</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 4</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 5</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+				</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ stock/index.php
@@ -0,0 +1,91 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Акции");
+$APPLICATION->SetPageProperty("bg-header", "baner-stock.jpg");
+
+?>
+
+<div class="sale-block-wrap">
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 1</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 2</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 3</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 4</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+					<a href="#" data="modal-sale" class="sale-block__el get-modal">
+						<div style="background-image:url('/img/sale-item.jpg')" class="sale-block__img">
+							<div class="sale-block-filter"></div>
+							<div class="block-date">
+								<div class="block-date__day">25</div>
+								<div class="block-date__time">
+									<div class="block-date__month">Сентября</div>
+									<div class="block-date__year">2016</div>
+								</div>
+							</div>
+						</div>
+						<div class="sale-block__info">
+							<div class="sale-block__title">Название акции 5</div>
+							<div class="sale-block__text">Евро ланч - специально создан для всех, кто предпочитает европейскую кухню! Европейский бизнес-ланч ....</div>
+						</div>
+					</a>
+				</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
